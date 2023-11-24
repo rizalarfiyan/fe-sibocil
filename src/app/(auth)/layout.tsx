bd2c75afe77b2c28a4a34b560e7cdd77922d6894
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-11-24 23:26:07 +0700

    feat: add graphic svg in login page
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import AuthGraphic from '@/assets/graphics/login.svg'
+
 export default function AuthLayout({
   children,
 }: {
@@ -5,8 +7,10 @@ export default function AuthLayout({
 }) {
   return (
     <div className='flex justify-center'>
-      <div className='hidden h-full min-h-screen flex-1 bg-secondary-100 md:block'></div>
-      <main className='relative h-full min-h-screen w-full max-w-xl lg:max-w-2xl'>
+      <div className='hidden h-full min-h-screen flex-1 bg-secondary-100 md:flex'>
+        <AuthGraphic className='mx-auto hidden w-full max-w-3xl lg:block' />
+      </div>
+      <main className='relative h-full min-h-screen w-full max-w-xl md:max-w-3xl lg:max-w-2xl'>
         <div className='flex min-h-screen w-full items-center justify-center p-10 py-24'>
           {children}
         </div>
