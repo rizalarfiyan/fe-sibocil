9ebc5eabb73e4c07602203776f6979aa76d4211e
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-12-10 15:41:02 +0700

    feat: add search in list of user
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/screen/Dashboard/User/Screen.tsx
+++ src/screen/Dashboard/User/Screen.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import { useRef } from 'react'
+import { Plus, Search } from 'lucide-react'
+import { useRef, useState } from 'react'
 
+import Button from '@/components/Button'
 import DataTable from '@/components/DataTable/DataTable'
 import {
   DataTableColumn,
   DataTableHandle,
 } from '@/components/DataTable/DataTable.types'
+import Input from '@/components/Input'
 import Typography from '@/components/Typography'
+import useDebounce from '@/hooks/useDebounce'
 
 import { getAll } from './service'
 
@@ -41,19 +45,43 @@ const columns: DataTableColumn = [
 
 const UserScreen: React.FC = () => {
   const tableRef = useRef<DataTableHandle>(null)
+  const [search, setSearch] = useState('')
+  const searchDebounce = useDebounce(search, 400)
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    setSearch(event.target.value)
+  }
 
   return (
-    <>
+    <div className='space-y-5'>
       <Typography variant='h2' as='h1'>
-        Get list of Users
+        List of Users
       </Typography>
+      <div className='flex flex-row items-center justify-between gap-2'>
+        <div className='relative'>
+          <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
+            <Search className='h-5 w-5 text-secondary-400' />
+          </div>
+          <Input
+            name='search'
+            value={search}
+            onChange={handleSearch}
+            placeholder='Search...'
+            className='max-w-sm pl-10'
+          />
+        </div>
+        <Button rightIcon={<Plus className='ml-1 h-5 w-5' />}>Add User</Button>
+      </div>
       <DataTable
         tableRef={tableRef}
         columns={columns}
         apiController={getAll}
         hasAutoNumber
+        query={{
+          search: searchDebounce || undefined,
+        }}
       />
-    </>
+    </div>
   )
 }
 
