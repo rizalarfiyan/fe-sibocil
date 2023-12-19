0f6d5c5a496779e6fe079aebf67c4efb8fe3273d
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-12-19 15:37:50 +0700

    feat: update wording in home page
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,23 @@ import Typography from '@/components/Typography'
 
 export default function Home() {
   return (
-    <div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-4'>
-      <Typography variant='h1' as='h1'>
-        Hello World!
-      </Typography>
-      <Button variant='outline' state='secondary' asChild>
-        <Link href='/login'>Login</Link>
-      </Button>
+    <div className='flex h-full min-h-screen w-full flex-col items-center justify-center gap-8'>
+      <div className='space-y-1 text-center'>
+        <Typography variant='h1' as='h1' className='text-secondary-800'>
+          Revend
+        </Typography>
+        <Typography variant='h4' as='h2' className='text-secondary-600'>
+          (Recycle Vending Machine)
+        </Typography>
+      </div>
+      <div className='flex items-center gap-2'>
+        <Button variant='outline' state='secondary' asChild>
+          <Link href='/login'>Login</Link>
+        </Button>
+        <Button variant='outline' state='secondary' asChild>
+          <Link href='/dashboard'>Dashboard</Link>
+        </Button>
+      </div>
     </div>
   )
 }
